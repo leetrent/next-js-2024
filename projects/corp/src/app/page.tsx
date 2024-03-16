62802276f68a9797962c7c1cb4e1341f224b656e
LeeTrent <lee.trent.1@gmail.com>
2024-03-16 10:26:56 -0500

    Adding images

--- projects/corp/src/app/page.tsx
+++ projects/corp/src/app/page.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image';
+import homeImg from '/public/home.jpg';
+
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div>
+      <span color="white">Home Page</span>
+      <div className="absolute -z-10 inset-0">
+        <Image
+        src={homeImg}
+        alt="Car Factory"
+        layout="fill"
+        objectFit="cover"
+        />
+      </div>
+    </div>
   );
 }
