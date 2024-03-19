746142e728f6558c146658d1c0f2101b13ecd163
LeeTrent <lee.trent.1@gmail.com>
2024-03-18 20:58:34 -0500

    Created corporation project and did not create a src folder.

---
+++ projects/corporation/app/page.tsx
@@ -0,0 +1,8 @@
+import homeImg from '/public/home.jpg';
+import Hero from '@/components/hero';
+
+export default function Home() {
+  return (
+    <Hero imgData={homeImg} imgAlt="Car Factory" title="Professional Cloud Hosting" />
+  );
+}
